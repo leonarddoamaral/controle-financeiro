ad69ecfd334175aa1c013d12759710de33b5c9aa
Leonardo do Amaral Quinquio <leonarddoamaral@gmail.com>
2026-08-04 20:42:52 -0300

    Alterando dependencias que ficaram faltando

--- main/src/test/java/com/FinMe/main/MainApplicationTests.java
+++ main/src/test/java/com/FinMe/main/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package com.FinMe.demo;
+package com.FinMe.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
